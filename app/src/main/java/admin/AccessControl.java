b378c2d679db21e75b6f48438005926d12952186
Piotr Polak <pepis@closbrothers.pl>
2015-09-27 04:29:04 +0200

    Admin access controll optimized, comments added

--- app/src/main/java/admin/AccessControl.java
+++ app/src/main/java/admin/AccessControl.java
@@ -9,52 +9,80 @@ package admin;
 
 import ro.polak.utilities.Config;
 import ro.polak.webserver.controller.MainController;
+import ro.polak.webserver.servlet.HTTPSession;
 
 public class AccessControl {
 
-    protected ro.polak.webserver.servlet.HTTPSession session;
+    protected HTTPSession session;
+    private static Config config;
 
-    public AccessControl(ro.polak.webserver.servlet.HTTPSession session) {
+    /**
+     * Default constructor
+     *
+     * @param session
+     */
+    public AccessControl(HTTPSession session) {
         this.session = session;
     }
 
+    /**
+     * Tells whether the user is logged
+     *
+     * @return
+     */
     public boolean isLogged() {
+        // There is no session active
         if (session == null) {
             return false;
         }
-        if (session.getAttribute("loggedin") == null) {
-            return false;
-        }
-        if (session.getAttribute("loggedin").equals("1")) {
+
+        // There must be an attribute loggedin and it must be equal 1
+        if (session.getAttribute("loggedin") != null && session.getAttribute("loggedin").equals("1")) {
             return true;
         }
+
         return false;
     }
 
+    /**
+     * Logs off the currently logged user
+     */
     public void logout() {
         session.setAttribute("loggedin", null);
     }
 
+    /**
+     * Logs the user in if the login and password match
+     *
+     * @param login
+     * @param password
+     * @return
+     */
     public boolean doLogin(String login, String password) {
         boolean logged = false;
         try {
+            // TODO Get the user from storage
             if (AccessControl.getConfig().get("_managementLogin").equals(login) && AccessControl.getConfig().get("_managementPassword").equals(password)) {
-
                 session.setAttribute("loggedin", "1");
                 logged = true;
-
-                // Get user from DB
             }
         } catch (NullPointerException e) {
-            // e.printStackTrace();
             logged = false;
         }
         return logged;
     }
 
+    /**
+     * Returns server config
+     *
+     * @return
+     */
     public static Config getConfig() {
-        Config config = new Config();
-        config.read(MainController.getInstance().getServer().getServerConfig().getBasePath() + "admin.conf");
+        // Initializes config only once
+        if (config == null) {
+            config = new Config();
+            config.read(MainController.getInstance().getServer().getServerConfig().getBasePath() + "admin.conf");
+        }
 
         return config;
     }

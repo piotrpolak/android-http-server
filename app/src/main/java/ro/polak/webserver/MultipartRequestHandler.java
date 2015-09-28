758a3634697c69ba19b93c5aed8595cc88715d1e
Piotr Polak <pepis@closbrothers.pl>
2015-09-28 02:35:21 +0200

    Improved readability and comments of MultipartRequestHandler

--- app/src/main/java/ro/polak/webserver/MultipartRequestHandler.java
+++ app/src/main/java/ro/polak/webserver/MultipartRequestHandler.java
@@ -42,8 +42,8 @@ public class MultipartRequestHandler {
     private byte[] buffer = new byte[2048];
     private StringBuffer headersStringBuffered = new StringBuffer();
     private StringBuffer valueStringBuffered;
-    private String boundary;
-    private String begin;
+    private String endBoundary;
+    private String beginBoundary;
     private String headersDeliminator = "\r\n\r\n";
     private String currentDeliminator;
     private MultipartHeaders mHeaders;
@@ -55,17 +55,19 @@ public class MultipartRequestHandler {
      *
      * @param in         the input stream
      * @param postLength expected length
-     * @param boundary   boundary string
+     * @param boundary   endBoundary string
      */
     public MultipartRequestHandler(InputStream in, int postLength, String boundary) {
-        this.mHeaders = new MultipartHeaders();
-        this.boundary = "\r\n--" + boundary;
-        this.begin = "--" + boundary;
+        mHeaders = new MultipartHeaders();
+
         this.postLength = postLength;
-        this.tempBuffer = new byte[this.boundary.length()];
+        endBoundary = "\r\n--" + boundary;
+        beginBoundary = "--" + boundary;
+
+        tempBuffer = new byte[endBoundary.length()];
         this.in = in;
-        this.allBytesRead = 0;
-        this.begin();
+        allBytesRead = 0;
+        begin();
     }
 
     /**
@@ -87,20 +89,47 @@ public class MultipartRequestHandler {
     }
 
     private void begin() {
-        boolean beginReached = false;
+        // Whether the boundary was completely read
+        boolean isBeginBoundaryCompletelyRead = false;
+        // Used for reading the input stream character by character
         byte[] smallBuffer = new byte[1];
 
-        while (!beginReached) {
+        while (!isBeginBoundaryCompletelyRead) {
             try {
+                /**
+                 *  The boundary, that is small enough, should be read character by character
+                 *  This is required, so that we do not need to trim/manipulate the remaining buffer
+                 */
                 bytesRead = in.read(smallBuffer);
+
+                // Unexpected end of buffer
                 if (bytesRead == -1) {
                     return;
                 }
+
+                // Incrementing the statistics
                 allBytesRead += bytesRead;
 
-                if (begin.charAt(charPosition) == smallBuffer[0]) {
-                    if (++charPosition == begin.length()) {
-                        beginReached = true;
+                /**
+                 * Start with the boundary character index=0
+                 * Reading character one by one
+                 *
+                 * If the currently read character matches the character at the current index
+                 *      then increment the index
+                 *      and check whether the begin boundary was completely read
+                 *      otherwise reset the boundary character index=0
+                 *
+                 * Then follow to the read body procedure
+                 *
+                 */
+                if (beginBoundary.charAt(charPosition) == smallBuffer[0]) {
+
+                    // Incrementing the boundary character index
+                    ++charPosition;
+
+                    // Check whether the begin boundary was completely read
+                    if (charPosition == beginBoundary.length()) {
+                        isBeginBoundaryCompletelyRead = true;
                         break;
                     }
                 } else {
@@ -111,7 +140,9 @@ public class MultipartRequestHandler {
                 //e.printStackTrace();
             }
         }
-        this.body();
+
+        // Follow to the read body procedure
+        body();
     }
 
     private void body() {
@@ -122,32 +153,30 @@ public class MultipartRequestHandler {
         bytesRead = charPosition = 0;
 
         try {
+            // Reading bytes into buffer Returning when out of new bytes
             while (true) {
 
-				/*
-                 * Reading bytes into buffer Returning when out of new bytes
-				 */
-
+                // Escaping when the real contents length is greater than the declared length
                 if (allBytesRead >= postLength) {
                     Statistics.addBytesReceived(allBytesRead);
                     break;
                 }
 
+                // Reading the input stream
                 bytesRead = in.read(buffer, 0, buffer.length);
+                // Incrementing the statistics
                 allBytesRead += bytesRead;
 
+                // No more bytes to read
                 if (bytesRead == -1) {
                     break;
                 }
 
                 begin = 0;
 
+                // For each read byte
                 for (int i = 0; i < bytesRead; i++) {
                     /*
-                     * For each read byte
-					 */
-
-					/*
                      * A kind of dynamic comparing of two string
 					 * 
 					 * Value of charPosition is automatically increased when
@@ -160,68 +189,59 @@ public class MultipartRequestHandler {
                          * Temp buffer is used in the case that there were some
 						 * positive comparisons at the end of the buffer, in
 						 * the case that the next read buffer contains chars
-						 * that are not the boundary, then this buffer is added
+						 * that are not the endBoundary, then this buffer is added
 						 * to the read string and furthermore processed. Else
 						 * this buffer is ignored
 						 */
                         tempBuffer[tempBufferCharPosition++] = buffer[i]; // buffering
 
-						/*
-                         * Checkig for the last character of the deliminator
-						 */
+                        // Checking for the last character of the deliminator
                         if (++charPosition == currentDeliminator.length()) {
-							/*
-							 * Swapping states header -> content OR content
-							 * ->header
+                            /*
+                             * Swapping states header -> content OR content -> header
 							 * 
 							 * Processing the last read (accepted) characters
 							 */
                             this.switchStates(begin, i - currentDeliminator.length());
 
-                            begin = i + 1; // Next first char is the next pos
-                            tempBufferCharPosition = 0; // Reseting, since no
-                            // longer needed
-                            wasPreviousBuffered = false; // No buffer needed for
-                            // the next steps
-                            charPosition = 0; // Reseting for pos of currend
-                            // compared char in the
-                            // deliminator
+                            // Next first char is the next pos
+                            begin = i + 1;
+                            // Resetting, since no longer needed
+                            tempBufferCharPosition = 0;
+                            // No buffer needed for the next steps
+                            wasPreviousBuffered = false;
+                            // Resetting for pos of current compared char in the deliminator
+                            charPosition = 0;
                         }
 
                     } else {
-						/*
-						 * This code is being executed if the currently compared
+                        /*
+                         * This code is being executed if the currently compared
 						 * char is not the "right one" This code is executed
 						 * only if the deliminator wasn't "closed"
 						 */
                         if (charPosition > 0) {
-							/* If there were any buffer */
+                            // If there were any buffer
                             if (wasPreviousBuffered) {
-								/*
-								 * If the buffer was activated in the last loop
-								 * Avoiding duplicationg of information
-								 */
+                                // If the buffer was activated in the last loop
+                                // Avoiding duplication of information
                                 this.releaseTempBuffer();
                                 wasPreviousBuffered = false;
                             }
                         }
 
-						/* Reseting positions */
+                        // Resetting positions
                         charPosition = 0;
                         tempBufferCharPosition = 0;
                     }
                 }
 
-				/*
-				 * This means that some buffer was recorded at the end of the buffer
-				 */
+                // This means that some buffer was recorded at the end of the buffer
                 if (charPosition > 0) {
-                    wasPreviousBuffered = true; // !THIS IS VALID FOR THE NEXT
-                    // LOOP ONLY
+                    // !THIS IS VALID FOR THE NEXT LOOP ONLY
+                    wasPreviousBuffered = true;
                 }
-				/*
-				 * Releasing the read buffer, excluding temp last bytes (see -charPosition)
-				 */
+                // Releasing the read buffer, excluding temp last bytes (see -charPosition)
                 this.releaseBuffer(begin, bytesRead - charPosition);
 
             }
@@ -229,74 +249,73 @@ public class MultipartRequestHandler {
             // TODO Throw exception instead of printing it
             //e.printStackTrace();
         }
+        // Removing the buffer out of memory
         buffer = null;
     }
 
     private void releaseTempBuffer() {
+
         if (tempBufferCharPosition == 0) {
             return; // Nothing new - exiting
         }
+
+        // Releasing headers
         if (headerReadingState) {
-			/*
-			 * This code is executed for the headers of multipart
-			 */
+            // This code is executed for the headers of multipart
             for (int i = 0; i < tempBufferCharPosition; i++) {
                 headersStringBuffered.append((char) tempBuffer[i]);
             }
-            return;
         }
-
-		/*
-		 * For the contents
-		 */
-
-        if (file != null) {
-            try {
-                fos.write(tempBuffer, 0, tempBufferCharPosition);
-            } // HERE IT IS OK, when changed, it really sucks
-            catch (IOException e) {
-                // TODO Throw exception instead of printing it
-                //e.printStackTrace();
-            }
-        } else {
-			/* For variables */
-            for (int i = 0; i < tempBufferCharPosition; i++) {
-                valueStringBuffered.append((char) buffer[i]);
+        // Else, releasing variable/file contents
+        else {
+            if (file != null) {
+                // This code is executed for the files
+                try {
+                    fos.write(tempBuffer, 0, tempBufferCharPosition);
+                } // HERE IT IS OK, when changed, it really sucks
+                catch (IOException e) {
+                    // TODO Throw exception instead of printing it
+                    //e.printStackTrace();
+                }
+            } else {
+                // For variables
+                for (int i = 0; i < tempBufferCharPosition; i++) {
+                    valueStringBuffered.append((char) buffer[i]);
+                }
             }
         }
     }
 
     private void releaseBuffer(int begin, int end) {
         int len = end - begin;
+
+        // Avoiding errors and exceptions
         if (len < 0) {
-            return; // Avoiding errors and exceptions
-        }
-        if (headerReadingState) {
-			/*
-			 * This code is executed for the headers of multipart
-			 */
-            for (int i = begin; i < end; i++) {
-                headersStringBuffered.append((char) buffer[i]);
-            }
             return;
         }
 
-		/*
-		 * For contents
-		 */
-
-        if (file != null) {
-			/* For file */
-            try {
-                fos.write(buffer, begin, end - begin);
-            } catch (IOException e) {
-                // TODO Throw exception instead of printing it
-                //e.printStackTrace();
-            }
-        } else {
-			/* For variables */
+        // Releasing headers
+        if (headerReadingState) {
+            // This code is executed for the headers of multipart
             for (int i = begin; i < end; i++) {
-                valueStringBuffered.append((char) buffer[i]);
+                headersStringBuffered.append((char) buffer[i]);
+            }
+        }
+        // Else, releasing variable/file contents
+        else {
+            if (file != null) {
+                // This code is executed for the files
+                try {
+                    fos.write(buffer, begin, end - begin);
+                } catch (IOException e) {
+                    // TODO Throw exception instead of printing it
+                    //e.printStackTrace();
+                }
+            } else {
+                // For variables
+                for (int i = begin; i < end; i++) {
+                    valueStringBuffered.append((char) buffer[i]);
+                }
             }
         }
     }
@@ -305,24 +324,18 @@ public class MultipartRequestHandler {
         int len = end - begin + 1;
 
         if (headerReadingState) {
-			/*
-			 * This code is executed for the headers of multipart
-			 */
+            // This code is executed for the headers of multipart
 
-			/*
-			 * Processing the remaining buffer Appending headers buffer
-			 */
+            // Processing the remaining buffer Appending headers buffer
             for (int i = begin; i <= end; i++) {
                 headersStringBuffered.append((char) buffer[i]);
             }
 
-			/*
-			 * Creating headers
-			 */
+            // Creating headers
             mHeaders.parse(headersStringBuffered.toString() + "\r");
 
             if (mHeaders.getContentType() != null) {
-				/* For files */
+                // For files
                 try {
                     file = new File(MainController.getInstance().getServer().getServerConfig().getTempPath() + RandomStringGenerator.generate());
                     fos = new FileOutputStream(file);
@@ -331,69 +344,56 @@ public class MultipartRequestHandler {
                     //e.printStackTrace();
                 }
             } else {
-				/* For values */
+                // For values
                 valueStringBuffered = new StringBuffer();
             }
 
-			/*
-			 * Switching to content state Changing boundary
-			 */
+            // Switching to content state Changing endBoundary
             headerReadingState = false;
-            currentDeliminator = boundary;
+            currentDeliminator = endBoundary;
 
-			/*
-			 * Reseting headers string buffer
-			 */
+            // Resetting headers string buffer
             headersStringBuffered = new StringBuffer();
 
-			/*
-			 * Returning, this is the end of the code for headers
-			 */
-            return;
-        }
+            // This is the end of the code for headers
+        } else {
+            // This code is executed for the content of multipart for both files and variables
+            // int len = end-beginBoundary-1;
 
-		/*
-		 * This code is executed for the content of multipart For files and
-		 * variables
-		 */
-        // int len = end-begin-1;
+            // Switching to content state Changing endBoundary
+            headerReadingState = true;
+            currentDeliminator = headersDeliminator;
 
-		/*
-		 * Switching to content state Changing boundary
-		 */
-        headerReadingState = true;
-        currentDeliminator = headersDeliminator;
+            if (file != null) {
+                // Write to file if any content exists Closing file stream
 
-        if (file != null) {
-			/*
-			 * Write to file if any content exists Closing file stream
-			 */
+                if (len > 0) {
+                    try {
+                        fos.write(buffer, begin, len);
+                    } catch (IOException e) {
+                        // TODO Throw exception instead of printing it
+                        //e.printStackTrace();
+                    }
+                }
 
-            if (len > 0) {
+                uploadedFiles.add(new UploadedFile(mHeaders, file));
+                // Resetting
+                file = null;
                 try {
-                    fos.write(buffer, begin, len);
-                } catch (IOException e) {
+                    fos.close();
+                } catch (Exception e) {
                     // TODO Throw exception instead of printing it
                     //e.printStackTrace();
                 }
-            }
-
-            uploadedFiles.add(new UploadedFile(mHeaders, file));
-            file = null; // Reseting
-            try {
-                fos.close();
-            } catch (Exception e) {
-                // TODO Throw exception instead of printing it
-                //e.printStackTrace();
-            }
-        } else {
-			/* For variables */
-            if (len > 0) {
-                for (int i = begin; i < end; i++) {
-                    valueStringBuffered.append(buffer[i]);
+            } else {
+                // For variables
+                if (len > 0) {
+                    for (int i = begin; i < end; i++) {
+                        valueStringBuffered.append(buffer[i]);
+                    }
                 }
+                this._post.put(mHeaders.getPostFieldName(), valueStringBuffered.toString());
             }
-            this._post.put(mHeaders.getPostFieldName(), valueStringBuffered.toString());
         }
     }
 }

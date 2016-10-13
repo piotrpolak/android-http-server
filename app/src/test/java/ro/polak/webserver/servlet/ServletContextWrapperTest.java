ac8f400ebc63ae5121abca6a1e6af2676374f390
Piotr Polak <piotr@polak.ro>
2016-10-14 01:10:41 +0200

    Implemented ServletContextWrapper session related tests

--- app/src/test/java/ro/polak/webserver/servlet/ServletContextWrapperTest.java
+++ app/src/test/java/ro/polak/webserver/servlet/ServletContextWrapperTest.java
@@ -8,19 +8,26 @@ import ro.polak.webserver.session.storage.SessionStorage;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.CoreMatchers.nullValue;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.lessThan;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.fail;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 public class ServletContextWrapperTest {
 
     @Test
-    public void shouldSetCookieForValidSession() throws IOException {
+    public void shouldSetCookieAndPersistForValidSession() throws IOException {
+        HttpSessionWrapper session = new HttpSessionWrapper("123");
         SessionStorage sessionStorage = mock(SessionStorage.class);
         ServletContextWrapper servletContext = new ServletContextWrapper(sessionStorage);
         HttpResponseWrapper response = new HttpResponseWrapper();
-        HttpSessionWrapper session = new HttpSessionWrapper("123");
         servletContext.handleSession(session, response);
+        verify(sessionStorage, times(1)).persistSession(session);
 
         for (Cookie cookie : response.getCookies()) {
             if (cookie.getName().equals(HttpSessionWrapper.COOKIE_NAME)) {
@@ -33,22 +40,54 @@ public class ServletContextWrapperTest {
     }
 
     @Test
-    public void shouldEraseCookieForInvalidSession() {
-        // TODO
+    public void shouldEraseCookieAndRemoveForInvalidatedSession() throws IOException {
+        HttpSessionWrapper session = new HttpSessionWrapper("123");
+        SessionStorage sessionStorage = mock(SessionStorage.class);
+        ServletContextWrapper servletContext = new ServletContextWrapper(sessionStorage);
+        HttpResponseWrapper response = new HttpResponseWrapper();
+        session.invalidate();
+        servletContext.handleSession(session, response);
+        verify(sessionStorage, times(1)).removeSession(session);
+
+        for (Cookie cookie : response.getCookies()) {
+            if (cookie.getName().equals(HttpSessionWrapper.COOKIE_NAME)) {
+                assertThat(cookie.getMaxAge(), lessThan(-1));
+                return;
+            }
+        }
+
+        fail("Session DELETE cookie was not set.");
     }
 
     @Test
-    public void shouldReturnSessionForValidSID() {
-        // TODO
+    public void shouldReturnSessionForValidSID() throws IOException {
+        SessionStorage sessionStorage = mock(SessionStorage.class);
+        HttpSessionWrapper session = new HttpSessionWrapper("123");
+        when(sessionStorage.getSession("123")).thenReturn(session);
+        ServletContextWrapper servletContext = new ServletContextWrapper(sessionStorage);
+        HttpSessionWrapper sessionRead = servletContext.getSession("123");
+        assertThat(sessionRead, is(not(nullValue())));
+        assertThat(sessionRead.getServletContext(), is(equalTo((ServletContext) servletContext)));
     }
 
     @Test
-    public void shouldRemoveExpiredSession() {
-        // TODO
+    public void shouldRemoveExpiredSession() throws IOException {
+        SessionStorage sessionStorage = mock(SessionStorage.class);
+        HttpSessionWrapper session = new HttpSessionWrapper("123");
+        session.setLastAccessedTime(System.currentTimeMillis() - session.getMaxInactiveInterval() * 1000);
+        when(sessionStorage.getSession("123")).thenReturn(session);
+        ServletContextWrapper servletContext = new ServletContextWrapper(sessionStorage);
+        HttpSessionWrapper sessionRead = servletContext.getSession("123");
+        verify(sessionStorage, times(1)).removeSession(session);
+        assertThat(sessionRead, is(nullValue()));
     }
 
     @Test
     public void shouldCreateSessionWithCorrectContext() {
-        // TODO
+        SessionStorage sessionStorage = mock(SessionStorage.class);
+        ServletContextWrapper servletContext = new ServletContextWrapper(sessionStorage);
+        HttpSessionWrapper session = servletContext.createNewSession();
+        assertThat(session, is(not(nullValue())));
+        assertThat(session.getServletContext(), is(equalTo((ServletContext) servletContext)));
     }
 }

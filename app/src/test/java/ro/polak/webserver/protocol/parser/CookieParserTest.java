2c946d48293e82bdc666e3335818e54f3ec6fc86
Piotr Polak <piotr@polak.ro>
2016-11-13 21:01:51 +0100

    Implemented CookieParserTest

--- app/src/test/java/ro/polak/webserver/protocol/parser/CookieParserTest.java
+++ app/src/test/java/ro/polak/webserver/protocol/parser/CookieParserTest.java
@@ -2,15 +2,32 @@ package ro.polak.webserver.protocol.parser;
 
 import org.junit.Test;
 
+import java.util.Map;
+
+import ro.polak.utilities.Utilities;
+import ro.polak.webserver.servlet.Cookie;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.collection.IsMapContaining.hasKey;
+import static org.junit.Assert.assertThat;
+
 public class CookieParserTest {
+
+    private static CookieParser cookieParser = new CookieParser();
 
     @Test
     public void shouldParseCookieHavingSpaceInValue() {
-        // TODO implement
+        String value = "value containing spaces";
+        Map<String, Cookie> cookies = cookieParser.parse("name=" + value);
+        assertThat(cookies, hasKey("name"));
+        assertThat(cookies.get("name").getValue(), is(value));
     }
 
     @Test
     public void shouldParseCookieHavingUrlEncodedValue() {
-        // TODO implement
+        String value = "&<>some value";
+        Map<String, Cookie> cookies = cookieParser.parse("name=" + Utilities.URLEncode(value));
+        assertThat(cookies, hasKey("name"));
+        assertThat(cookies.get("name").getValue(), is(value));
     }
 }

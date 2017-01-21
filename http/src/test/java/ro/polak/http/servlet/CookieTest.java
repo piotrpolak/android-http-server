e3a663047ce6e6e1408c47ed68c72fe4f4a7002d
Piotr Polak <piotr@polak.ro>
2017-01-21 01:08:51 +0100

    Improved CookieTest

--- http/src/test/java/ro/polak/http/servlet/CookieTest.java
+++ http/src/test/java/ro/polak/http/servlet/CookieTest.java
@@ -2,10 +2,37 @@ package ro.polak.http.servlet;
 
 import org.junit.Test;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
 public class CookieTest {
 
     @Test(expected = IllegalArgumentException.class)
     public void shouldNotAllowIllegalName() {
         new Cookie(";illegal", "somevalue");
+    }
+
+    @Test
+    public void shouldAllowBooleanValues() {
+        Cookie cookie = new Cookie("someName", true);
+        assertThat(cookie.getValue(), is("true"));
+    }
+
+    @Test
+    public void shouldAllowIntValues() {
+        Cookie cookie = new Cookie("someName", 14);
+        assertThat(cookie.getValue(), is("14"));
+    }
+
+    @Test
+    public void shouldAllowLongValues() {
+        Cookie cookie = new Cookie("someName", 1545454454544844l);
+        assertThat(cookie.getValue(), is("1545454454544844"));
+    }
+
+    @Test
+    public void shouldAllowDoubleValues() {
+        Cookie cookie = new Cookie("someName", 22.33);
+        assertThat(cookie.getValue(), is("22.33"));
     }
 }

3ed536f91aaa49e0c4aaa141d714c74534c9ed79
Piotr Polak <piotr@polak.ro>
2017-09-02 00:50:53 +0200

    Webserver now runs on a GUI independent thread

--- app/src/main/java/ro/polak/webserver/MainActivity.java
+++ app/src/main/java/ro/polak/webserver/MainActivity.java
@@ -2,50 +2,125 @@
  * Android Web Server
  * Based on JavaLittleWebServer (2008)
  * <p/>
- * Copyright (c) Piotr Polak 2008-2015
+ * Copyright (c) Piotr Polak 2008-2017
  **************************************************/
 
 package ro.polak.webserver;
 
 import android.app.AlertDialog;
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.DialogInterface;
-import android.net.wifi.WifiInfo;
-import android.net.wifi.WifiManager;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
-
-import java.math.BigInteger;
-import java.net.InetAddress;
-import java.net.NetworkInterface;
-import java.net.SocketException;
-import java.util.Enumeration;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
-import javax.net.ServerSocketFactory;
+import android.widget.Toast;
 
 import ro.polak.http.R;
-import ro.polak.http.controller.Controller;
-import ro.polak.http.controller.MainController;
-import ro.polak.http.gui.ServerGui;
-import ro.polak.webserver.resource.provider.impl.AndroidServerConfigFactory;
 
 /**
  * The main server Android activity
+ *
+ * @author Piotr Polak piotr [at] polak [dot] ro
+ * @since 201008
  */
-public class MainActivity extends AppCompatActivity implements ServerGui {
+public class MainActivity extends AppCompatActivity {
 
-    private static final Logger LOGGER = Logger.getLogger(MainActivity.class.getName());
-
-    private TextView status, ipText, consoleText;
-    private Button actionButton, backgroundButton, quitButton;
+    private TextView status;
+    private TextView ipText;
+    private TextView consoleText;
+    private Button actionButton;
+    private Button backgroundButton;
+    private Button quitButton;
     private ImageView imgView;
-    private Controller controller;
+
+    private MainService mainService;
+    private boolean isMainServiceBound = false;
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Intent intent = new Intent(this, MainService.class);
+        bindService(intent, serviceConnection, Context.BIND_AUTO_CREATE);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (isMainServiceBound) {
+            unbindService(serviceConnection);
+            isMainServiceBound = false;
+        }
+    }
+
+    private ServiceConnection serviceConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName componentName, IBinder iBinder) {
+            MainService.LocalBinder binder = (MainService.LocalBinder) iBinder;
+            mainService = binder.getService();
+            mainService.registerClient(MainActivity.this);
+            isMainServiceBound = true;
+
+            if (!mainService.getServiceState().isServicetarted()) {
+                Intent serviceIntent = new Intent(MainActivity.this, MainService.class);
+                startService(serviceIntent);
+            }
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName arg0) {
+            isMainServiceBound = false;
+        }
+    };
+
+    public void informStateChanged() {
+        if (isMainServiceBound) {
+            MainService.ServiceState serviceState = mainService.getServiceState();
+            if (serviceState.isWebSercerStarted()) {
+                println("Starging HTTPD");
+                ipText.setText(serviceState.getAccessUrl());
+
+                imgView.setImageResource(R.drawable.online);
+                actionButton.setVisibility(View.VISIBLE);
+                actionButton.setText("Stop HTTPD");
+                status.setText("Server online");
+            } else {
+                println("Stopping HTTPD");
+                imgView.setImageResource(R.drawable.offline);
+                status.setText("Server offline");
+                actionButton.setVisibility(View.VISIBLE);
+                actionButton.setText("Start HTTPD");
+            }
+        }
+
+
+    }
+
+    /**
+     * GUI print debug method
+     *
+     * @param text
+     */
+    public void println(final String text) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    consoleText.setText(text + "\n" + consoleText.getText());
+                } catch (Exception e) {
+                    Log.i("HTTP", text);
+                }
+            }
+        });
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -67,12 +142,6 @@ public class MainActivity extends AppCompatActivity implements ServerGui {
         quitButton.setOnClickListener(new ButtonListener(this));
 
         status.setText("Initializing");
-
-        controller = new MainController(new AndroidServerConfigFactory(this),
-                ServerSocketFactory.getDefault(),
-                this);
-        
-        controller.start();
     }
 
     /**
@@ -98,8 +167,22 @@ public class MainActivity extends AppCompatActivity implements ServerGui {
                         .setCancelable(false)
                         .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
                             public void onClick(DialogInterface dialog, int id) {
-                                activity.controller.stop();
-                                activity.finish();
+                                if (isMainServiceBound) {
+                                    Handler mainHandler = new Handler(getApplicationContext().getMainLooper());
+                                    mainHandler.post(new Runnable() {
+                                        @Override
+                                        public void run() {
+
+                                            if (mainService.getServiceState().isServicetarted()) {
+                                                Intent serviceIntent = new Intent(MainActivity.this, MainService.class);
+                                                stopService(serviceIntent);
+                                            }
+                                        }
+                                    });
+                                    activity.finish();
+                                } else {
+                                    Toast.makeText(getApplicationContext(), "Not bound", Toast.LENGTH_SHORT).show();
+                                }
                             }
                         })
                         .setNegativeButton("No", new DialogInterface.OnClickListener() {
@@ -110,104 +193,16 @@ public class MainActivity extends AppCompatActivity implements ServerGui {
                 builder.create().show();
 
             } else if (id == actionButton.getId()) {
-                try {
-                    if (activity.controller.getWebServer() != null && activity.controller.getWebServer().isRunning()) {
-                        activity.controller.stop();
+                if (isMainServiceBound) {
+                    if (mainService.getServiceState().isWebSercerStarted()) {
+                        mainService.getController().stop();
                     } else {
-                        activity.controller.start();
+                        mainService.getController().start();
                     }
-                } catch (Exception e) {
-                    activity.controller.start();
+                } else {
+                    Toast.makeText(getApplicationContext(), "Not bound", Toast.LENGTH_SHORT).show();
                 }
             }
-
         }
-    }
-
-    /**
-     * GUI method called by controller on start
-     */
-    public void start() {
-        println("Starging HTTPD");
-        status.setText("Starting...");
-
-        String ip = getLocalIpAddress();
-        if (ip == null) {
-            ip = "127.0.0.1";
-        }
-        int port = controller.getWebServer().getServerConfig().getListenPort();
-        String portString = "";
-        if (port != 80) {
-            portString = ":" + port;
-        }
-        ipText.setText("http://" + ip + portString + '/');
-
-        imgView.setImageResource(R.drawable.online);
-        actionButton.setVisibility(View.VISIBLE);
-        actionButton.setText("Stop HTTPD");
-        status.setText("Server online");
-    }
-
-    /**
-     * GUI method called by controller on stop
-     */
-    public void stop() {
-        println("Stopping HTTPD");
-        imgView.setImageResource(R.drawable.offline);
-        status.setText("Server offline");
-        actionButton.setVisibility(View.VISIBLE);
-        actionButton.setText("Start HTTPD");
-    }
-
-    /**
-     * GUI print debug method
-     *
-     * @param text
-     */
-    public void println(final String text) {
-        runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    consoleText.setText(text + "\n" + consoleText.getText());
-                } catch (Exception e) {
-                    Log.i("HTTP", text);
-                }
-            }
-        });
-    }
-
-    /**
-     * Helper
-     *
-     * @return String
-     */
-    public String getLocalIpAddress() {
-        try {
-            WifiManager wifiManager = (WifiManager) getSystemService(WIFI_SERVICE);
-            WifiInfo wifiInfo = wifiManager.getConnectionInfo();
-
-            int ipAddress = wifiInfo.getIpAddress();
-            ipAddress = (java.nio.ByteOrder.nativeOrder().equals(java.nio.ByteOrder.LITTLE_ENDIAN)) ? Integer.reverseBytes(ipAddress) : ipAddress;
-            InetAddress inetAddress = InetAddress.getByAddress(BigInteger.valueOf(ipAddress).toByteArray());
-            return inetAddress.getHostAddress().toString();
-
-        } catch (Exception e) {
-            try {
-                for (Enumeration<NetworkInterface> en = NetworkInterface.getNetworkInterfaces(); en.hasMoreElements(); ) {
-                    NetworkInterface intf = en.nextElement();
-                    for (Enumeration<InetAddress> enumIpAddr = intf.getInetAddresses(); enumIpAddr.hasMoreElements(); ) {
-                        InetAddress inetAddress = enumIpAddr.nextElement();
-                        if (!inetAddress.isLoopbackAddress()) {
-                            return inetAddress.getHostAddress().toString();
-                        }
-                    }
-                }
-            } catch (SocketException ex) {
-                LOGGER.log(Level.SEVERE, "Unable to obtain own IP address", e);
-            }
-        }
-
-        return null;
     }
 }
---
+++ app/src/main/java/ro/polak/webserver/MainService.java
@@ -0,0 +1,215 @@
+/**************************************************
+ * Android Web Server
+ * Based on JavaLittleWebServer (2008)
+ * <p/>
+ * Copyright (c) Piotr Polak 2008-2017
+ **************************************************/
+
+package ro.polak.webserver;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Intent;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
+import android.os.Binder;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+import android.text.format.Formatter;
+
+import java.math.BigInteger;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.Enumeration;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.net.ServerSocketFactory;
+
+import ro.polak.http.R;
+import ro.polak.http.controller.Controller;
+import ro.polak.http.controller.MainController;
+import ro.polak.http.gui.ServerGui;
+import ro.polak.webserver.resource.provider.impl.AndroidServerConfigFactory;
+
+/**
+ * Main application service that holds http server
+ *
+ * @author Piotr Polak piotr [at] polak [dot] ro
+ * @since 201709
+ */
+public class MainService extends Service implements ServerGui {
+
+    private static final Logger LOGGER = Logger.getLogger(MainService.class.getName());
+    public static final int NOTIFICATION_ID = 0;
+
+    @Nullable
+    private MainActivity activity = null;
+    private Controller controller;
+    private final IBinder binder = new LocalBinder();
+    private boolean isServiceStarted = false;
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return binder;
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        isServiceStarted = true;
+
+        controller = new MainController(new AndroidServerConfigFactory(this),
+                ServerSocketFactory.getDefault(),
+                this
+        );
+        controller.start();
+
+        return START_STICKY;
+    }
+
+    @Override
+    public void onDestroy() {
+        if (getServiceState().isWebSercerStarted()) {
+            controller.stop();
+        }
+
+        isServiceStarted = false;
+        NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        notificationManager.cancel(NOTIFICATION_ID);
+    }
+
+    public void registerClient(MainActivity activity) {
+        this.activity = activity;
+    }
+
+    public Controller getController() {
+        return controller;
+    }
+
+    public ServiceState getServiceState() {
+        ServiceState serviceState = new ServiceState();
+
+        String accessUrl = "Initializing";
+        if (controller != null && controller.getWebServer() != null) {
+            int port = controller.getWebServer().getServerConfig().getListenPort();
+            String portString = port != 80 ? ":" + port : "";
+            accessUrl = "http://" + getLocalIpAddress() + portString + '/';
+        }
+
+        serviceState.setAccessUrl(accessUrl);
+        serviceState.setServicetarted(isServiceStarted);
+        serviceState.setWebSercerStarted(controller != null && controller.getWebServer() != null && controller.getWebServer().isRunning());
+
+        return serviceState;
+    }
+
+    @Override
+    public void start() {
+        if (activity != null) {
+            activity.informStateChanged();
+        }
+
+        Intent notificationIntent = new Intent(this, MainActivity.class);
+        PendingIntent pIntent = PendingIntent.getActivity(this, 0, notificationIntent, 0);
+        setNotification(getNotificationBuilder(pIntent, "Started", R.drawable.online).build());
+    }
+
+    @Override
+    public void stop() {
+        if (activity != null) {
+            activity.informStateChanged();
+        }
+
+        Intent notificationIntent = new Intent(this, MainActivity.class);
+        PendingIntent pIntent = PendingIntent.getActivity(this, 0, notificationIntent, 0);
+        setNotification(getNotificationBuilder(pIntent, "Stopped", R.drawable.offline).build());
+    }
+
+    private void setNotification(Notification notification) {
+        NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        notificationManager.notify(NOTIFICATION_ID, notification);
+    }
+
+    private Notification.Builder getNotificationBuilder(PendingIntent pIntent, String text, int icon) {
+        return new Notification.Builder(this)
+                .setContentTitle("HTTPServer")
+                .setContentText(text)
+                .setSmallIcon(icon)
+                .setContentIntent(pIntent)
+                .setOngoing(true)
+                .addAction(R.drawable.online, "Open", pIntent);
+    }
+
+    /**
+     * Helper
+     *
+     * @return String
+     */
+    public String getLocalIpAddress() {
+        try {
+            WifiManager wifiManager = (WifiManager) getSystemService(WIFI_SERVICE);
+            WifiInfo wifiInfo = wifiManager.getConnectionInfo();
+
+            int ipAddress = wifiInfo.getIpAddress();
+            ipAddress = (java.nio.ByteOrder.nativeOrder().equals(java.nio.ByteOrder.LITTLE_ENDIAN)) ? Integer.reverseBytes(ipAddress) : ipAddress;
+            InetAddress inetAddress = InetAddress.getByAddress(BigInteger.valueOf(ipAddress).toByteArray());
+            return inetAddress.getHostAddress();
+
+        } catch (Exception e) {
+            try {
+                for (Enumeration<NetworkInterface> en = NetworkInterface.getNetworkInterfaces(); en.hasMoreElements(); ) {
+                    NetworkInterface intf = en.nextElement();
+                    for (Enumeration<InetAddress> enumIpAddr = intf.getInetAddresses(); enumIpAddr.hasMoreElements(); ) {
+                        InetAddress inetAddress = enumIpAddr.nextElement();
+                        if (!inetAddress.isLoopbackAddress()) {
+                            return Formatter.formatIpAddress(inetAddress.hashCode());
+                        }
+                    }
+                }
+            } catch (SocketException ex) {
+                LOGGER.log(Level.SEVERE, "Unable to obtain own IP address", e);
+            }
+        }
+
+        return "127.0.0.1";
+    }
+
+    public class LocalBinder extends Binder {
+        public MainService getService() {
+            return MainService.this;
+        }
+    }
+
+    public class ServiceState {
+        private boolean isServicetarted;
+        private boolean isWebSercerStarted;
+        private String accessUrl = "";
+
+        public boolean isServicetarted() {
+            return isServicetarted;
+        }
+
+        public void setServicetarted(boolean servicetarted) {
+            isServicetarted = servicetarted;
+        }
+
+        public boolean isWebSercerStarted() {
+            return isWebSercerStarted;
+        }
+
+        public void setWebSercerStarted(boolean webSercerStarted) {
+            isWebSercerStarted = webSercerStarted;
+        }
+
+        public String getAccessUrl() {
+            return accessUrl;
+        }
+
+        public void setAccessUrl(String accessUrl) {
+            this.accessUrl = accessUrl;
+        }
+    }
+}

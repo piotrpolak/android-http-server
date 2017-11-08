e6a89066a9238c6739e11aaddb1271f1b622b0d4
Piotr Polak <piotr@polak.ro>
2017-11-09 00:00:10 +0100

    Implemented basic DateProviderTest

---
+++ http/src/test/java/ro/polak/http/utilities/DateProviderTest.java
@@ -0,0 +1,19 @@
+package ro.polak.http.utilities;
+
+import org.junit.Test;
+
+import java.util.Date;
+
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.lessThanOrEqualTo;
+import static org.junit.Assert.assertThat;
+
+public class DateProviderTest {
+
+    @Test
+    public void shouldReturnAValidDate() throws Exception {
+        DateProvider dateProvider = new DateProvider();
+        assertThat(dateProvider.now().getTime(), is(lessThanOrEqualTo(new Date().getTime())));
+    }
+
+}

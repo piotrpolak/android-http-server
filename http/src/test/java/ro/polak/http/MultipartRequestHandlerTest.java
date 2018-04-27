a4ea7228b8e0dff9e19af7dbd3e4764976220258
Piotr Polak <piotr@polak.ro>
2018-04-27 23:33:56 +0200

    Improved data preparation for MultipartRequestHandlerTest

--- http/src/test/java/ro/polak/http/MultipartRequestHandlerTest.java
+++ http/src/test/java/ro/polak/http/MultipartRequestHandlerTest.java
@@ -22,7 +22,6 @@ import static org.junit.Assert.fail;
 public class MultipartRequestHandlerTest {
 
     private static final String BOUNDARY = "------------BOUNDARY";
-    private static final String NEW_LINE = "\r\n";
     private static final String TEMPORARY_UPLOADS_DIRECTORY
             = System.getProperty("java.io.tmpdir") + "/";
     private static final Parser<MultipartHeadersPart> parser = new MultipartHeadersPartParser(
@@ -31,31 +30,11 @@ public class MultipartRequestHandlerTest {
 
     @Test
     public void shouldParseBasicFields() throws MalformedInputException {
-        String data =
-                "--" + BOUNDARY +
-                        NEW_LINE +
-                        "Content-Disposition: form-data; name=\"field_1\"" +
-                        NEW_LINE +
-                        NEW_LINE +
-                        "A123" +
-                        NEW_LINE +
-                        "--" + BOUNDARY +
-                        NEW_LINE +
-                        "Content-Disposition: form-data; name=\"field_2\"" +
-                        NEW_LINE +
-                        NEW_LINE +
-                        "B123" +
-                        NEW_LINE +
-                        "--" + BOUNDARY +
-                        NEW_LINE +
-                        "Content-Disposition: form-data; name=\"field_3\"" +
-                        NEW_LINE +
-                        NEW_LINE +
-                        "C123" +
-                        NEW_LINE +
-                        "--" + BOUNDARY +
-                        NEW_LINE +
-                        NEW_LINE;
+        String data = new MultipartInputBuilder(BOUNDARY)
+                .withField("field_1", "A123")
+                .withField("field_2", "B123")
+                .withField("field_3", "C123")
+                .build();
 
         MultipartRequestHandler mrh = new MultipartRequestHandler(parser, getStreamOutOfString(data),
                 data.length(), BOUNDARY, TEMPORARY_UPLOADS_DIRECTORY, 2048);
@@ -75,31 +54,11 @@ public class MultipartRequestHandlerTest {
 
     @Test
     public void shouldParseWhenBoundaryIsLikeFieldValues() throws MalformedInputException {
-        String data =
-                "--" + BOUNDARY +
-                        NEW_LINE +
-                        "Content-Disposition: form-data; name=\"field_0\"" +
-                        NEW_LINE +
-                        NEW_LINE +
-                        "--------------BOUNDAR" +
-                        NEW_LINE +
-                        "--" + BOUNDARY +
-                        NEW_LINE +
-                        "Content-Disposition: form-data; name=\"field_1\"" +
-                        NEW_LINE +
-                        NEW_LINE +
-                        "------------BOUNDARY" +
-                        NEW_LINE +
-                        "--" + BOUNDARY +
-                        NEW_LINE +
-                        "Content-Disposition: form-data; name=\"field_2\"" +
-                        NEW_LINE +
-                        NEW_LINE +
-                        "------------BOUNDARY-" +
-                        NEW_LINE +
-                        "--" + BOUNDARY +
-                        NEW_LINE +
-                        NEW_LINE;
+        String data = new MultipartInputBuilder(BOUNDARY)
+                .withField("field_0", "--------------BOUNDAR")
+                .withField("field_1", "------------BOUNDARY")
+                .withField("field_2", "------------BOUNDARY-")
+                .build();
 
         MultipartRequestHandler mrh = new MultipartRequestHandler(parser, getStreamOutOfString(data),
                 data.length(), BOUNDARY, TEMPORARY_UPLOADS_DIRECTORY, 2048);
@@ -120,27 +79,10 @@ public class MultipartRequestHandlerTest {
 
     @Test
     public void shouldHandleBasicFileUpload() throws MalformedInputException {
-        String data =
-                "--" + BOUNDARY +
-                        NEW_LINE +
-                        "Content-Disposition: form-data; name=\"field_1\"" +
-                        NEW_LINE +
-                        NEW_LINE +
-                        "A123" +
-                        NEW_LINE +
-                        "--" + BOUNDARY +
-                        NEW_LINE +
-                        "Content-Disposition: attachment; name=\"FIELDNAME\"; " +
-                        "filename=\"FILE.PDF\"" +
-                        NEW_LINE +
-                        "Content-type: application/pdf" +
-                        NEW_LINE +
-                        NEW_LINE +
-                        "ABCD" +
-                        NEW_LINE +
-                        "--" + BOUNDARY +
-                        NEW_LINE +
-                        NEW_LINE;
+        String data = new MultipartInputBuilder(BOUNDARY)
+                .withField("field_1", "A123")
+                .withFile("FIELDNAME", "FILE.PDF", "application/pdf", "ABCD")
+                .build();
 
         MultipartRequestHandler mrh = new MultipartRequestHandler(parser, getStreamOutOfString(data),
                 data.length(), BOUNDARY, TEMPORARY_UPLOADS_DIRECTORY, 2048);
@@ -160,37 +102,11 @@ public class MultipartRequestHandlerTest {
 
     @Test
     public void shouldHandleFileUploadForEmptyFile() throws MalformedInputException {
-        String data =
-                "--" + BOUNDARY +
-                        NEW_LINE +
-                        "Content-Disposition: form-data; name=\"field_1\"" +
-                        NEW_LINE +
-                        NEW_LINE +
-                        "A123" +
-                        NEW_LINE +
-                        "--" + BOUNDARY +
-                        NEW_LINE +
-                        "Content-Disposition: attachment; name=\"FIELDNAME_00\"; " +
-                        "filename=\"FILE.PDF\"" +
-                        NEW_LINE +
-                        "Content-type: application/pdf" +
-                        NEW_LINE +
-                        NEW_LINE +
-                        "" +
-                        NEW_LINE +
-                        "--" + BOUNDARY +
-                        NEW_LINE +
-                        "Content-Disposition: attachment; name=\"FIELDNAME_01\"; " +
-                        "filename=\"FILE.PDF\"" +
-                        NEW_LINE +
-                        "Content-type: application/pdf" +
-                        NEW_LINE +
-                        NEW_LINE +
-                        "ABCD" +
-                        NEW_LINE +
-                        "--" + BOUNDARY +
-                        NEW_LINE +
-                        NEW_LINE;
+        String data = new MultipartInputBuilder(BOUNDARY)
+                .withField("field_1", "A123")
+                .withFile("FIELDNAME_00", "FILE.PDF", "application/pdf", "")
+                .withFile("FIELDNAME_01", "FILE.PDF", "application/pdf", "ABCD")
+                .build();
 
         MultipartRequestHandler mrh = new MultipartRequestHandler(parser, getStreamOutOfString(data),
                 data.length(), BOUNDARY, TEMPORARY_UPLOADS_DIRECTORY, 2048);
@@ -216,94 +132,22 @@ public class MultipartRequestHandlerTest {
     public void shouldHandleFieldsOnBufferMargin() throws MalformedInputException {
         // TODO Implement similar test for file upload to see whether delayed write works fine
         String shortBoundary = "%";
-        String data =
-                "--" + shortBoundary +
-                        NEW_LINE +
-                        "Content-Disposition: form-data; name=\"field_00\"" +
-                        NEW_LINE +
-                        NEW_LINE +
-                        "" +
-                        NEW_LINE +
-                        "--" + shortBoundary +
-                        NEW_LINE +
-                        "Content-Disposition: form-data; name=\"field_01\"" +
-                        NEW_LINE +
-                        NEW_LINE +
-                        "A" +
-                        NEW_LINE +
-                        "--" + shortBoundary +
-                        NEW_LINE +
-                        "Content-Disposition: form-data; name=\"field_02\"" +
-                        NEW_LINE +
-                        NEW_LINE +
-                        "BB" +
-                        NEW_LINE +
-                        "--" + shortBoundary +
-                        NEW_LINE +
-                        "Content-Disposition: form-data; name=\"field_03\"" +
-                        NEW_LINE +
-                        NEW_LINE +
-                        "CCC" +
-                        NEW_LINE +
-                        "--" + shortBoundary +
-                        NEW_LINE +
-                        "Content-Disposition: form-data; name=\"field_04\"" +
-                        NEW_LINE +
-                        NEW_LINE +
-                        "DDDD" +
-                        NEW_LINE +
-                        "--" + shortBoundary +
-                        NEW_LINE +
-                        "Content-Disposition: form-data; name=\"field_05\"" +
-                        NEW_LINE +
-                        NEW_LINE +
-                        "EEEEE" +
-                        NEW_LINE +
-                        "--" + shortBoundary +
-                        NEW_LINE +
-                        "Content-Disposition: form-data; name=\"field_06\"" +
-                        NEW_LINE +
-                        NEW_LINE +
-                        "FFFFFF" +
-                        NEW_LINE +
-                        "--" + shortBoundary +
-                        NEW_LINE +
-                        "Content-Disposition: form-data; name=\"field_07\"" +
-                        NEW_LINE +
-                        NEW_LINE +
-                        "GGGGGGG" +
-                        NEW_LINE +
-                        "--" + shortBoundary +
-                        NEW_LINE +
-                        "Content-Disposition: form-data; name=\"field_08\"" +
-                        NEW_LINE +
-                        NEW_LINE +
-                        "HHHHHHHH" +
-                        NEW_LINE +
-                        "--" + shortBoundary +
-                        NEW_LINE +
-                        "Content-Disposition: form-data; name=\"field_09\"" +
-                        NEW_LINE +
-                        NEW_LINE +
-                        "IIIIIIIII" +
-                        NEW_LINE +
-                        "--" + shortBoundary +
-                        NEW_LINE +
-                        "Content-Disposition: form-data; name=\"field_10\"" +
-                        NEW_LINE +
-                        NEW_LINE +
-                        "JJJJJJJJJJ" +
-                        NEW_LINE +
-                        "--" + shortBoundary +
-                        NEW_LINE +
-                        "Content-Disposition: form-data; name=\"field_11\"" +
-                        NEW_LINE +
-                        NEW_LINE +
-                        "KKKKKKKKKKK" +
-                        NEW_LINE +
-                        "--" + shortBoundary +
-                        NEW_LINE +
-                        NEW_LINE;
+
+        String data = new MultipartInputBuilder(shortBoundary)
+                .withField("field_00", "")
+                .withField("field_01", "A")
+                .withField("field_02", "BB")
+                .withField("field_03", "CCC")
+                .withField("field_04", "DDDD")
+                .withField("field_05", "EEEEE")
+                .withField("field_06", "FFFFFF")
+                .withField("field_07", "GGGGGGG")
+                .withField("field_08", "HHHHHHHH")
+                .withField("field_09", "IIIIIIIII")
+                .withField("field_10", "JJJJJJJJJJ")
+                .withField("field_11", "KKKKKKKKKKK")
+                .build();
+
 
         MultipartRequestHandler mrh = new MultipartRequestHandler(parser, getStreamOutOfString(data),
                 data.length(), shortBoundary, TEMPORARY_UPLOADS_DIRECTORY, 1);
@@ -329,31 +173,11 @@ public class MultipartRequestHandlerTest {
 
     @Test(expected = IllegalStateException.class)
     public void shouldBeHandledOnceOnly() throws MalformedInputException {
-        String data =
-                "--" + BOUNDARY +
-                        NEW_LINE +
-                        "Content-Disposition: form-data; name=\"field_1\"" +
-                        NEW_LINE +
-                        NEW_LINE +
-                        "A123" +
-                        NEW_LINE +
-                        "--" + BOUNDARY +
-                        NEW_LINE +
-                        "Content-Disposition: form-data; name=\"field_2\"" +
-                        NEW_LINE +
-                        NEW_LINE +
-                        "B123" +
-                        NEW_LINE +
-                        "--" + BOUNDARY +
-                        NEW_LINE +
-                        "Content-Disposition: form-data; name=\"field_3\"" +
-                        NEW_LINE +
-                        NEW_LINE +
-                        "C123" +
-                        NEW_LINE +
-                        "--" + BOUNDARY +
-                        NEW_LINE +
-                        NEW_LINE;
+        String data = new MultipartInputBuilder(BOUNDARY)
+                .withField("field_1", "A123")
+                .withField("field_2", "B123")
+                .withField("field_3", "C123")
+                .build();
 
         MultipartRequestHandler mrh = new MultipartRequestHandler(parser, getStreamOutOfString(data),
                 data.length(), BOUNDARY, TEMPORARY_UPLOADS_DIRECTORY, 2048);
@@ -369,8 +193,7 @@ public class MultipartRequestHandlerTest {
     @Test(expected = IOException.class)
     public void shouldThrowIOExceptionOnEndedBoundary()
             throws MalformedInputException, IOException {
-        String data =
-                "--123";
+        String data = "--123";
 
         MultipartRequestHandler mrh = new MultipartRequestHandler(parser, getStreamOutOfString(data),
                 data.length() * 2, BOUNDARY, TEMPORARY_UPLOADS_DIRECTORY, 2048);
@@ -381,16 +204,11 @@ public class MultipartRequestHandlerTest {
     @Test(expected = PayloadTooLargeProtocolException.class)
     public void shouldStopParsingOnWrongContentLengthInBeforeBoundary()
             throws MalformedInputException {
-        String data = "----------------------------------------------------------" + BOUNDARY +
-                NEW_LINE +
-                "Content-Disposition: form-data; name=\"field_1\"" +
-                NEW_LINE +
-                NEW_LINE +
-                "A123" +
-                NEW_LINE +
-                "--" + BOUNDARY +
-                NEW_LINE +
-                NEW_LINE;
+
+        String data = "--------------------------------------------------------" +
+                new MultipartInputBuilder(BOUNDARY)
+                        .withField("field_1", "A123")
+                        .build();
 
         MultipartRequestHandler mrh = new MultipartRequestHandler(parser, getStreamOutOfString(data), 5,
                 BOUNDARY, TEMPORARY_UPLOADS_DIRECTORY, 2048);
@@ -405,30 +223,14 @@ public class MultipartRequestHandlerTest {
 
     @Test(expected = PayloadTooLargeProtocolException.class)
     public void shouldStopParsingOnWrongContentLengthInBody() throws MalformedInputException {
-        String begin = "--" + BOUNDARY +
-                NEW_LINE +
-                "Content-Disposition: form-data; name=\"field_1\"" +
-                NEW_LINE +
-                NEW_LINE +
-                "A123" +
-                NEW_LINE;
+        String begin = new MultipartInputBuilder(BOUNDARY)
+                .withField("field_1", "A123")
+                .build();
 
-        String data = begin +
-                "--" + BOUNDARY +
-                NEW_LINE +
-                "Content-Disposition: form-data; name=\"field_2\"" +
-                NEW_LINE +
-                NEW_LINE +
-                "B123" + NEW_LINE +
-                "--" + BOUNDARY + NEW_LINE +
-                "Content-Disposition: form-data; name=\"field_3\"" +
-                NEW_LINE +
-                NEW_LINE +
-                "C123" +
-                NEW_LINE +
-                "--" + BOUNDARY +
-                NEW_LINE +
-                NEW_LINE;
+        String data = begin + new MultipartInputBuilder(BOUNDARY)
+                .withField("field_2", "B123")
+                .withField("field_3", "C123")
+                .build();
 
         MultipartRequestHandler mrh = new MultipartRequestHandler(parser, getStreamOutOfString(data),
                 begin.length(), BOUNDARY, TEMPORARY_UPLOADS_DIRECTORY, 2048);
@@ -442,5 +244,55 @@ public class MultipartRequestHandlerTest {
 
     private InputStream getStreamOutOfString(String data) {
         return new ByteArrayInputStream(data.getBytes(StandardCharsets.UTF_8));
+    }
+
+    /**
+     * Builds multipart input.
+     */
+    private static class MultipartInputBuilder {
+
+        private static final String NEW_LINE = "\r\n";
+        private StringBuilder contents = new StringBuilder();
+        private String boundary;
+
+        private MultipartInputBuilder(String boundary) {
+            this.boundary = boundary;
+
+            addBoundary();
+        }
+
+        private void addBoundary() {
+            contents.append("--").append(boundary).append(NEW_LINE);
+        }
+
+        public MultipartInputBuilder withField(String fieldName, String fieldValue) {
+            contents.append("Content-Disposition: form-data; name=\"").append(fieldName).append("\"")
+                    .append(NEW_LINE)
+                    .append(NEW_LINE)
+                    .append(fieldValue)
+                    .append(NEW_LINE);
+            addBoundary();
+
+            return this;
+        }
+
+        public MultipartInputBuilder withFile(String fieldName, String fileName, String contentType, String fileContents) {
+            contents.append("Content-Disposition: attachment; name=\"").append(fieldName).append("\"; ")
+                    .append("filename=\"").append(fileName).append("\"")
+                    .append(NEW_LINE)
+                    .append("Content-type: ").append(contentType)
+                    .append(NEW_LINE)
+                    .append(NEW_LINE)
+                    .append(fileContents)
+                    .append(NEW_LINE);
+            addBoundary();
+
+            return this;
+        }
+
+        public String build() {
+            contents.append(NEW_LINE);
+            return contents.toString();
+        }
     }
 }

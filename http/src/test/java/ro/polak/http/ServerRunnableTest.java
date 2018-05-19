54d18d0a6db4b1eb08f2319d1beeb2c4d52cbbb7
Piotr Polak <piotr@polak.ro>
2018-05-19 23:14:56 +0200

    Covered ServerRunnable logging with tests

---
+++ http/src/test/java/ro/polak/http/ServerRunnableTest.java
@@ -0,0 +1,39 @@
+package ro.polak.http;
+
+import org.junit.Test;
+
+import java.io.IOException;
+import java.net.Socket;
+
+import ro.polak.http.configuration.ServerConfig;
+import ro.polak.http.errorhandler.HttpErrorHandlerResolver;
+import ro.polak.http.servlet.factory.HttpServletRequestImplFactory;
+import ro.polak.http.servlet.factory.HttpServletResponseImplFactory;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+public class ServerRunnableTest {
+
+    @Test
+    public void shouldLogIOExceptionsSilently() throws Exception {
+
+        Socket socket = mock(Socket.class);
+        HttpServletResponseImplFactory responseFactory = mock(HttpServletResponseImplFactory.class);
+
+        when(responseFactory.createFromSocket(socket)).thenThrow(new IOException());
+
+        ServerRunnable serverRunnable = new ServerRunnable(socket, mock(ServerConfig.class),
+                mock(HttpServletRequestImplFactory.class),
+                responseFactory,
+                mock(HttpErrorHandlerResolver.class),
+                new PathHelper()
+        );
+
+        serverRunnable.run();
+
+        verify(socket, times(1)).close();
+    }
+}

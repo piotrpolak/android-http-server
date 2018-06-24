bfb7ce668e6d948bc794420230b5490299bcb567
Piotr Polak <piotr@polak.ro>
2018-06-24 15:05:35 +0200

    Improved LocaleParser

--- http/src/main/java/ro/polak/http/protocol/parser/impl/LocaleParser.java
+++ http/src/main/java/ro/polak/http/protocol/parser/impl/LocaleParser.java
@@ -8,6 +8,7 @@
 package ro.polak.http.protocol.parser.impl;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Locale;
 
@@ -24,6 +25,7 @@ public class LocaleParser implements Parser<List<Locale>> {
 
     private static final String LOCALE_SEPARATOR = ",";
     private static final String WEIGHT_SEPARATOR = ";";
+    public static final String WEIGHT_PREFIX = "q=";
 
     /**
      * A very basic implementation of locale parser that ignores weights.
@@ -35,16 +37,67 @@ public class LocaleParser implements Parser<List<Locale>> {
     @Override
     public List<Locale> parse(String input) throws MalformedInputException {
         String localesStr[] = input.split(LOCALE_SEPARATOR);
-        List<Locale> locales = new ArrayList<>();
+        List<LocaleWithWeight> localesWithWeight = new ArrayList<>();
+
         for (String locale : localesStr) {
+            double weight = 1.0;
             String[] localeParts = locale.split(WEIGHT_SEPARATOR);
+
+            if (localeParts.length == 0) {
+                continue;
+            }
+
             if (localeParts[0].length() != 2) {
                 continue;
             }
 
-            locales.add(new Locale(localeParts[0]));
+            if (localeParts.length > 1) {
+                if (localeParts[1].length() > 0) {
+                    if (localeParts[1].startsWith(WEIGHT_PREFIX)) {
+                        weight = Double.parseDouble(localeParts[1].substring(2));
+                    } else {
+                        continue;
+                    }
+                }
+            }
+
+            localesWithWeight.add(new LocaleWithWeight(new Locale(localeParts[0]), weight));
+        }
+
+        return toLocales(localesWithWeight);
+    }
+
+    private List<Locale> toLocales(List<LocaleWithWeight> localesWithWeight) {
+        Collections.sort(localesWithWeight);
+
+        List<Locale> locales = new ArrayList<>();
+        for (LocaleWithWeight localeWithWeight : localesWithWeight) {
+            locales.add(localeWithWeight.getLocale());
         }
 
         return locales;
+    }
+
+    private static class LocaleWithWeight implements Comparable {
+        private Locale locale;
+        private double weight;
+
+        public LocaleWithWeight(Locale locale, double weight) {
+            this.locale = locale;
+            this.weight = weight;
+        }
+
+        @Override
+        public int compareTo(Object o) {
+            return Double.compare(((LocaleWithWeight) o).getWeight(), getWeight());
+        }
+
+        public Locale getLocale() {
+            return locale;
+        }
+
+        public double getWeight() {
+            return weight;
+        }
     }
 }
--- http/src/test/java/ro/polak/http/protocol/parser/impl/LocaleParserTest.java
+++ http/src/test/java/ro/polak/http/protocol/parser/impl/LocaleParserTest.java
@@ -24,4 +24,48 @@ public class LocaleParserTest {
         assertThat(locales.get(2), is(new Locale("ro")));
         assertThat(locales.get(3), is(new Locale("ru")));
     }
+
+    @Test
+    public void shouldParseLocalesAndRespectWeights() throws MalformedInputException {
+        String localeString = "pl-PL,ru;q=0.2,en;q=0.4,ro;q=0.3,pl;q=0.8";
+        List<Locale> locales = LOCALE_PARSER.parse(localeString);
+
+        assertThat(locales.get(0), is(new Locale("pl")));
+        assertThat(locales.get(1), is(new Locale("en")));
+        assertThat(locales.get(2), is(new Locale("ro")));
+        assertThat(locales.get(3), is(new Locale("ru")));
+    }
+
+    @Test
+    public void shouldHandleProperlyMissingWeight() throws MalformedInputException {
+        String localeString = "pl-PL,ru;q=0.2,en;q=0.4,ro;q=0.3,pl";
+        List<Locale> locales = LOCALE_PARSER.parse(localeString);
+
+        assertThat(locales.get(0), is(new Locale("pl")));
+        assertThat(locales.get(1), is(new Locale("en")));
+        assertThat(locales.get(2), is(new Locale("ro")));
+        assertThat(locales.get(3), is(new Locale("ru")));
+    }
+
+    @Test
+    public void shouldIgnoreInvalidWeights() throws MalformedInputException {
+        String localeString = "pl-PL,fr;TT,ru;q=0.2,en;q=0.4,ro;q=0.3,pl";
+        List<Locale> locales = LOCALE_PARSER.parse(localeString);
+
+        assertThat(locales.get(0), is(new Locale("pl")));
+        assertThat(locales.get(1), is(new Locale("en")));
+        assertThat(locales.get(2), is(new Locale("ro")));
+        assertThat(locales.get(3), is(new Locale("ru")));
+    }
+
+    @Test
+    public void shouldHandleErrorsTransparently() throws MalformedInputException {
+        String localeString = "pl-PL,pl;,;;,,ru;q=0.2,en;q=0.4,ro;q=0.3";
+        List<Locale> locales = LOCALE_PARSER.parse(localeString);
+
+        assertThat(locales.get(0), is(new Locale("pl")));
+        assertThat(locales.get(1), is(new Locale("en")));
+        assertThat(locales.get(2), is(new Locale("ro")));
+        assertThat(locales.get(3), is(new Locale("ru")));
+    }
 }

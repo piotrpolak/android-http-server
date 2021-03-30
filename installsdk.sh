39b85cdee9f4da62ccdeb4487b9bf6a796f29fc6
Piotr Polak <piotr@polak.ro>
2021-03-30 23:29:07 +0200

    SDK installation scripts updated

--- installsdk.sh
+++ installsdk.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-wget https://dl.google.com/android/repository/sdk-tools-linux-4333796.zip -P/tmp && \
-    unzip /tmp/sdk-tools-linux-4333796.zip -d ~/android-sdk && \
-    unlink /tmp/sdk-tools-linux-4333796.zip && \
+wget https://dl.google.com/android/repository/commandlinetools-linux-6858069_latest.zip -P/tmp && \
+    unzip /tmp/commandlinetools-linux-6858069_latest.zip -d ~/android-sdk && \
+    unlink /tmp/commandlinetools-linux-6858069_latest.zip && \
     export ANDROID_HOME=~/android-sdk && \
+    mv $ANDROID_HOME/cmdline-tools/ $ANDROID_HOME/latest && \
+    mkdir $ANDROID_HOME/cmdline-tools/ && \
+    mv $ANDROID_HOME/latest $ANDROID_HOME/cmdline-tools/latest && \
     mkdir -p ~/.android && \
     touch ~/.android/repositories.cfg && \
-    yes | $ANDROID_HOME/tools/bin/sdkmanager "platform-tools" "platforms;android-26"
+    yes | $ANDROID_HOME/cmdline-tools/latest/bin/sdkmanager "platform-tools" "platforms;android-26" && \
+    yes | $ANDROID_HOME/cmdline-tools/latest/bin/sdkmanager --licenses
